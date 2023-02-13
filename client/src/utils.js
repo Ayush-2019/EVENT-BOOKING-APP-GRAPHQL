d556d166432df4fea86429017ee085e155985de1
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2023-02-13 12:12:21 +0530

    Delete client directory

--- client/src/utils.js
+++
@@ -1,4 +0,0 @@
-export const token = localStorage.getItem("token");
-export const userID = localStorage.getItem("userID");
-export const tokenexpire = localStorage.getItem("tokenexpire");
-export const bookingList = localStorage.getItem("bookings");
